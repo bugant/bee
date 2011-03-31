1528465e53afa1bd1ffc3d073d77945205a64963
Matteo Centenaro <bugant@gmail.com>
2011-03-31 11:37:00 +0200

    adding a simple example generating an RSA key-pair using     opencryptoki

---
+++ examples/generate-rsa-pair.c
@@ -0,0 +1,40 @@
+/*
+ * This is a simple example generating an RSA key pair
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <bee.h>
+
+int main()
+{
+    bee b;
+    CK_OBJECT_HANDLE pub, priv;
+    CK_BBOOL yes = TRUE;
+    CK_BBOOL no = FALSE;
+    CK_ULONG len = 1024;
+    attrs pub_foo, priv_foo;
+
+    if (init("libopencryptoki.so", "12345", &b) != CKR_OK)
+    {
+	printf("too bad: cannot init my bee!\n");
+	return 1;
+    }
+
+    init_attrs(&pub_foo);
+    add_bool_attr(&pub_foo, CKA_TOKEN, &yes);
+    set_default_pub_template(&b, &pub_foo);
+
+    init_attrs(&priv_foo);
+    add_bool_attr(&priv_foo, CKA_TOKEN, &yes);
+    set_default_priv_template(&b, &priv_foo);
+
+    if (generate_key_pair(b, 1024, &pub, &priv) != CKR_OK)
+    {
+	printf("too bad: cannot generate a key-pair\n");
+	return 1;
+    }
+    printf("you have a new key-pair\n");
+
+    return;
+}
